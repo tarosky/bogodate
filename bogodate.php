ce5f728d8285984fcfa47195c888f72124d13057
Ko Takagi <knockoutmarch@gmail.com>
2018-04-26 16:01:37 +0900

    update comment

--- bogodate.php
+++ bogodate.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:     Bogo Date
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/tarosky/bogodate
+ * Description:     Date format as a Bogo extension for WordPress.
+ * Author:          tarosky, ko31
+ * Author URI:      https://tarosky.co.jp/
  * Text Domain:     bogodate
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
  * @package         Bogo_Date
  */
